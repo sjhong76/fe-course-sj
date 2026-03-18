c85eb329feb0c1513f3459df0db239b79c19116b
sjhong76 <sjhong76@gmail.com>
2026-03-18 16:58:00 +0900

    20260318 commit

---
+++ javascript/hello.js
@@ -0,0 +1,13 @@
+//한 줄 주석
+/*
+    hello.js -> 자바스크립트 엔진(Node.js)에서 바로 실행
+*/
+let a = 100;
+let b = 200;
+let sum = a + b;
+console.log('a =', a);
+console.log('b =', b);
+console.log('sum =', sum);
+console.log('a + b =', (a + b));
+let name = '홍길동'
+console.log(name);
