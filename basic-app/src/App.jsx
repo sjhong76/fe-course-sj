831fed3afe33bb6549c19669dfa0a91a0f43b907
sjhong76 <sjhong76@gmail.com>
2026-04-08 18:22:33 +0900

    20260408 commit

--- basic-app/src/App.jsx
+++ basic-app/src/App.jsx
@@ -1,21 +1,42 @@
-import './App.css';   //전역 CSS
-import style from './App.module.css'; 
-import Body from './components/Body.jsx';
-import Parent from './components/Parent.jsx';
-import Child from './components/Child.jsx';
+import AvatarImage from "./components/avatar/AvatarImage.jsx";
+import AvatarImageList from "./components/avatar/AvatarImageList.jsx";
+import Avatar from "./components/avatar/Avatar.jsx";
+import AvatarList from "./components/avatar/AvatarList.jsx";
+import style from './components/avatar/Avatar.module.css';
+import MenuList from './components/menu/MenuList.jsx';
+import mstyle from './components/menu/Menu.module.css';
+import people1 from './assets/people1.webp';
+import people2 from './assets/people2.webp';
+import people3 from './assets/people3.webp';
 
+export default function App() {
+  const list = [
+    {img: people1, style:style.avatar_img},
+    {img: people2, style:style.avatar_img},
+    {img: people3, style:style.avatar_img_circle},
+    {img: people3, style:style.avatar_img}
+  ]
+  const alist = [
+    {img: people1, name: "Smith" },
+    {img: people2, name: "James"},
+    {img: people3, name: "Anne"},
+    {img: people3, name: "Anne"}
+  ]
+  const mlist = [
+    {title: "멤버십", href: "#", style: mstyle.menu1},
+    {title: "고객센터", href: "#", style: mstyle.menu1},
+    {title: "단체관람/대관문의", href: "#", style: mstyle.menu1},
+    {title: "로그인", href: "#", style: mstyle.menu1},
+  ]
 
-
-function App() {
-
-  return (   //화면에 출력하는 것이 있다.
+  return ( 
     <>
-      <h1>Hello~ React!!</h1>
-      <button className={style.button}>App::클릭하세요</button>
-      <Body />
-      <Parent name="홍길동 아버지" />
+      <AvatarImage img={people1} style={style.avatar_img}/>
+      <Avatar name="Anne" img={people3} />
+      <AvatarImageList imgList={list} />
+      <AvatarList list={alist} />
+      <MenuList list={mlist} />
     </>
   )
 }
 
-export default App
